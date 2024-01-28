89badc608e39c483e3465f4abf79f3116e812192
ben hackmon <ben.hackmon@gmail.com>
2024-01-29 01:10:07 +0200

    product select filter

--- src/Pages/ProductPage/components/ProductDetails.tsx
+++ src/Pages/ProductPage/components/ProductDetails.tsx
@@ -1,4 +1,6 @@
 import { Button, Typography, useTheme } from '@mui/material';
+import { useState } from 'react';
+import { includes, isEmpty, some } from 'lodash';
 import { FullColumn, Row } from '../../../Layout';
 import { data as product } from '../../../api/product.json';
 import Amount from './Amount';
@@ -8,12 +10,61 @@ type ProductDetailsProps = {
     product: typeof product
 }
 
+// const getProductVarientByLabelId = (productsVariant: typeof product.variants, attrID: string, labelId: string) =>
+//     productsVariant.filter(x => x.labels.some(x => x.label_id === labelId && x.attribute_id === attrID))
+
+// const getLableIdsByAttributeId = (productsVariant: typeof product.variants, attrId: string) =>
+//     productsVariant.map(({ labels }) => labels.find(x => x.attribute_id === attrId)?.label_id)
+
+// const productsVariant = getProductVarientByLabelId(product.variants, '1', '4')
+// const labelIds = getLableIdsByAttributeId(productsVariant, '2')
+// console.log(productsVariant, labelIds)
+
+// console.log(product.attributes.find(x => x.id === '2')?.labels.filter(label => labelIds.includes(label.id)))
+
+// console.table(product.attributes)
+// console.table(product.variants.map(({ id, labels, title }) => ({ id, labels, title })));
+
 const ProductDetails = ({ product }: ProductDetailsProps) => {
     const { breakpoints: { down } } = useTheme();
+    const [selectedAttributes, setSelectedAttributes] = useState<Record<string, string>>({});
+    console.log(selectedAttributes);
+    console.log(product.variants);
+
+    const handleAttributeChange = (attributeId: string, labelId: string) => {
+        setSelectedAttributes((prevSelected) => ({
+            ...prevSelected,
+            [attributeId]: labelId,
+        }));
+    };
+
+    const deleteAttribute = (attributeId: string) => {
+        setSelectedAttributes((prevSelected) => {
+            const attributesCopy = { ...prevSelected }
+            delete attributesCopy[attributeId]
+            return attributesCopy
+        }
+        );
+    };
+
+    const getAvailableOptions = (attributeId: string) => {
+        console.log('---------------------------------------------------------------------------------');
+
+        const availableOptions = product.attributes.find((attribute) => attribute.id === attributeId)?.labels.filter(label => {
+            const availableVariants = product.variants.filter(variant => (Object.entries(selectedAttributes)
+                .map(([attribute_id, label_id]) => ({ attribute_id, label_id })))
+                .every(val => includes(variant.labels.map(x => x.label_id), val.label_id)))
+                .some(x => x.labels.some(x => x.label_id === label.id));
+
+            return isEmpty(selectedAttributes) ? label : availableVariants;
+        })
+
+        return availableOptions;
+    }
 
     return (
         <FullColumn sx={{
-            flex: 2, gap: 1, padding: 1,
+            flex: 2, gap: 1.5, padding: 3,
             overflow: 'auto',
             [down('sm')]: {
                 flex: 8,
@@ -43,7 +94,13 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
             {
                 product.attributes.map(attr =>
                     <Row key={attr.id} sx={{ width: 0.5, minWidth: 150 }}>
-                        <ProductSelectField attr={attr} />
+                        <ProductSelectField
+                            attr={attr}
+                            value={selectedAttributes[attr.id] || ''}
+                            onChange={({ target: { value: labelId } }) => handleAttributeChange(attr.id, labelId)}
+                            availableOptions={getAvailableOptions(attr.id)!}
+                            onDelete={deleteAttribute}
+                        />
                     </Row>
                 )
             }
